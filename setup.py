2dc34f9f50ed9666898b290bc97d8d976c09368d
Arnav Malhotra <emumalhotra@gmail.com>
2023-08-04 18:36:28 -0400

    Quadratic equation solver

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='complex_mathematics',
-    version='1.2.1',
+    version='2.2.1',
     packages=find_packages(),
     install_requires=[
         'numpy', 'scipy'
