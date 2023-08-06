da22a2c7df3bfea2703d338b38838f49f853ac14
Arnav Malhotra <emumalhotra@gmail.com>
2023-08-06 14:58:24 -0400

    Fixed quadratic

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='complex_mathematics',
-    version='2.2.1',
+    version='2.3.3',
     packages=find_packages(),
     install_requires=[
         'numpy', 'scipy'
@@ -12,6 +12,6 @@ setup(
     author="Arnav Malhotra",
     author_email="emumalhotra@gmail.com",
     description="Many complex math features.",
-    long_description="Many advanced math features and topics like linear algebra, geometry, calculus, algebra, and some machine learning algorithms.",
+    long_description="Many advanced math features and topics like linear algebra, geometry, calculus, algebra, and machine learning algorithms.",
     url="https://github.com/Arnav-MaIhotra/complex_mathematics",
 )
