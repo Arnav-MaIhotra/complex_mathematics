d8029880fb69bb985657e215a5d60ee2c36851b2
Arnav Malhotra <emumalhotra@gmail.com>
2023-08-16 16:20:45 -0400

    Linear regression normal equations

--- build/lib/complex_mathematics/ml.py
+++ build/lib/complex_mathematics/ml.py
@@ -3,12 +3,13 @@ import math
 
 class LinearRegression:
 
-  def __init__(self, learning_rate = 0.01, max_iters = 100000, tolerance = 1e-10):
+  def __init__(self, learning_rate = 0.01, max_iters = 100000, tolerance = 1e-10, optimization_method = "SGD"):
     self.learning_rate = learning_rate
     self.max_iters = max_iters
     self.tolerance = tolerance
     self.params = None
     self.bias = None
+    self.om = optimization_method
 
   def h(self, X):
         return np.dot(X, self.params) + self.bias
@@ -17,47 +18,49 @@ class LinearRegression:
       se = np.mean((self.h(X) - y) ** 2)/2
       return se
 
-  def fit(self, X, Y, progress=False):
-    self.num_features = X.shape[1]
-    self.num_samples = X.shape[0]
+  def fit(self, X, Y):
+    if self.om == "SGD":
+      self.num_features = X.shape[1]
+      self.num_samples = X.shape[0]
 
-    self.params = np.zeros(self.num_features)
-    self.bias = 0
+      self.params = np.zeros(self.num_features)
+      self.bias = 0
 
-    if Y.size != self.num_samples:
-        raise ValueError("Number of samples in X and Y do not match.")
+      if Y.size != self.num_samples:
+          raise ValueError("Number of samples in X and Y do not match.")
 
-    iters = 0
+      iters = 0
 
-    prev_cost = float('inf')
+      prev_cost = float('inf')
 
-    for _ in range(self.max_iters):
-      index = np.random.randint(self.num_samples)
-      
-      x = X[index]
-      y = Y[index]
+      for _ in range(self.max_iters):
+        index = np.random.randint(self.num_samples)
+        
+        x = X[index]
+        y = Y[index]
 
-      predicted = np.dot(x, self.params) + self.bias
+        predicted = np.dot(x, self.params) + self.bias
 
-      grad_coef = (2/self.num_samples) * np.dot(x.T, predicted - y)
-      grad_inte = (2/self.num_samples) * np.sum(predicted - y)
+        grad_coef = (2/self.num_samples) * np.dot(x.T, predicted - y)
+        grad_inte = (2/self.num_samples) * np.sum(predicted - y)
 
-      self.params -= self.learning_rate * grad_coef
-      self.bias -= self.learning_rate * grad_inte
+        self.params -= self.learning_rate * grad_coef
+        self.bias -= self.learning_rate * grad_inte
 
-      current_cost = self.j(X, Y)
+        current_cost = self.j(X, Y)
 
-      if abs(current_cost - prev_cost) < self.tolerance:
-          break
+        if abs(current_cost - prev_cost) < self.tolerance:
+            break
 
-      prev_cost = current_cost
+        prev_cost = current_cost
 
-      if progress:
-          print("Squared error: ", self.j(X, Y))
-          iters += 1
+    elif self.om == "NormalEquations":
+      X = np.c_[np.ones((X.shape[0], 1)), X]
 
-    if progress:
-       print("Iterations: ", iters)
+      des = np.linalg.inv(X.T.dot(X)).dot(X.T.dot(y))
+
+      self.bias = des[0]
+      self.params = des[1:]
 
   def predict(self, x):
     return np.dot(x, self.params) + self.bias
